f24719c4b4c6056fc037f41e65ea0fc3011ca288
Jason Lin <jasonlin@Jasons-MacBook-Pro.local>
2017-06-15 17:48:19 -0400

    part 3: raising exception

--- polls/views.py
+++ polls/views.py
@@ -2,17 +2,32 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import Http404
+from .models import Question
 
 
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	# template = loader.get_template('polls/index.html')
+	context = {'latest_question_list': latest_question_list }
+	return render(request, 'polls/index.html', context)
+	# output = ', '.join([q.question_text for q in latest_question_list])
+	# this is the old lengthy way: 
+	# return HttpResponse(template.render(context, request))
+	# return HttpResponse("Hello, world. You're at the polls index.")
 	
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	try: 
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/detail.html', {'question': question})
+	# return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
 	# what is %s referring to in our case? A: see line 20
